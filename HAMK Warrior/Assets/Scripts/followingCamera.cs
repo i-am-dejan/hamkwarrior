74ea4951d72f00fda1fae98a755d22d8a1869d6b
TPA2018 <43805671+TPA2018@users.noreply.github.com>
2018-10-12 13:24:29 +0300

    Following Camera
    +Dragged Main Camera from Player back to Hierarchy list as a parent object
    +Made new script to Scripts-folder named followingCamera
    +drag-and-dropped followingCamera script to Main Camera (and attached Player gameobject to script)
    +Adjusted Main Camera settings (centered camera to player, changed Size value to 4)
    
    +Fixed flickering blue line problem
    ** change from Grid gameobject's Cell Gap values: X: -0.01 and Y: -0.01
    
    +Made new folder named Scripts
    ** Moved moving-script in Scripts-folder
    
    + Some changes in PlayerSettings:
    ** Allowewd orientations for auto rotation: landscape right & landscape left
    ** Identification - Package Name: com.HAMK.HAMKWarrior

---
+++ HAMK Warrior/Assets/Scripts/followingCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followingCamera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
