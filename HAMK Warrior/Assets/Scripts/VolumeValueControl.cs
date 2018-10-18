83480ac89a406e96dc16ddabe88e844a8144fc17
Dejan Trifunovic <dejan.trifunovic@student.hamk.fi>
2018-10-19 01:41:44 +0300

    add sound and backgroundsound regulation

---
+++ HAMK Warrior/Assets/Scripts/VolumeValueControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValueControl : MonoBehaviour
+{
+
+    private AudioSource audioSrc;
+    public float musicVolumeStart = 0.64f;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSrc.volume = musicVolumeStart;
+    }
+
+    // Takes Volume value passed by slider
+    public void SetVolume(float vol)
+    {
+        musicVolumeStart = vol;
+        VolumeHandler.getInstance().setVolume(musicVolumeStart);
+    }
+
+    public float getVolume()
+    {
+        return musicVolumeStart;
+    }
+
+}
