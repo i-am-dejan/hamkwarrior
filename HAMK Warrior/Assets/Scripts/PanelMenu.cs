d3c8cd1b88f05a32d51262bfaf8b290f31449f5b
Dejan Trifunovic <dejan.trifunovic@student.hamk.fi>
2018-10-23 19:17:11 +0300

    updates

---
+++ HAMK Warrior/Assets/Scripts/PanelMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMenu : MonoBehaviour {
+ 
+
+	public void StartGame ()
+    {
+        GameObject panel = GameObject.FindGameObjectWithTag("Panel");
+        panel.active = false;
+        Debug.Log("Button gedrückt.");
+        // Time.timeScale = 1.0f;
+        //Panel.GetComponentInChildren<Image>().enabled = false;
+        //Panel.renderer.enabled = false;
+    }
+}
