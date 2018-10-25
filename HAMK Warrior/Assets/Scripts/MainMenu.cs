a7338d74fcc70d3017bab73871701480b4ef0d18
Dejan Trifunovic <dejan.trifunovic@student.hamk.fi>
2018-10-25 10:13:24 +0300

    fixed score by using retry after dead

--- HAMK Warrior/Assets/Scripts/MainMenu.cs
+++ HAMK Warrior/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,8 @@ public class MainMenu : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 0.0f;
         moving.collectedDiamonds = 0;
+        moving.collectedDiamondsTotally = 0;
+
     }
 
     public void QuitGame ()
